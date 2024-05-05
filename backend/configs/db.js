e5df1c5cb2efe008008be266b0f37b605c9a8d1f
Aditya Verma <vermaaditya0707@gmail.com>
2024-05-05 18:22:09 +0530

    updated

--- backend/configs/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connection = mongoose.connect(
-  "mongodb+srv://vermaaditya0707:0707@cluster0.i8ihmzs.mongodb.net/ecommerce"
-);
-
-module.exports = {
-  connection,
-};
